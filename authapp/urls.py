64ca06765c06750dd7f9d987533633c31b254b31
GregoryVins <gregoryvins@gmail.com>
2021-01-25 14:16:42 +0300

    user login:    - added form with bootstrap class    - added URL    - added links in templates (login/register)    - added class UserLoginView with logic

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from authapp.views import UserRegisterView
+from authapp.views import UserRegisterView, UserLoginView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='user_register'),
+    path('login/', UserLoginView.as_view(), name='user_login'),
 ]
--- authapp/views.py
+++ authapp/views.py
@@ -1,16 +1,38 @@
+from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView
 
-from authapp.forms import HabrUserRegisterForm
+from authapp.forms import HabrUserRegisterForm, HabrUserLoginForm
 from authapp.models import HabrUser
 
 
 class UserRegisterView(CreateView):
     """
     Регистрация пользователя.
-    После успешной регистрации перенаправляет пользователя на главную страницу.
+    После успешной регистрации перенаправляет пользователя на страницу входа.
     """
     form_class = HabrUserRegisterForm
     template_name = 'authapp/register.html'
-    success_url = reverse_lazy('list_articles')
+    success_url = reverse_lazy('user_login')
     model = HabrUser
+
+
+class UserLoginView(FormView):
+    """
+    Авторизация пользователя.
+    После успешной авторизации перенаправляет пользователя на главную страницу.
+    """
+    form_class = HabrUserLoginForm
+    template_name = 'authapp/login.html'
+    success_url = reverse_lazy('list_articles')
+
+    def form_valid(self, form):
+        username = self.request.POST['username']
+        password = self.request.POST['password']
+        print(username, password)
+        userauth = authenticate(self.request, username=username, password=password)
+        print(userauth)
+        if userauth is not None:
+            if userauth.is_active:
+                login(self.request, userauth)
+        return super(UserLoginView, self).form_valid(form)
