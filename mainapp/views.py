1b92391be352891a5ef2d3ff1762533d74ade167
Sergei Makarenko <makarenko.s@inbox.ru>
2021-01-26 17:57:32 +0500

    Added article detail view. Fix registration button

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from mainapp.models import Category, Article
 
@@ -7,6 +7,17 @@ class ArticleListView(ListView):
     model = Article
     template_name = 'mainapp/index.html'
     context_object_name = 'articles'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories_list'] = Category.objects.all()
+        return context
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'mainapp/article_detail.html'
+    context_object_name = 'article'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
