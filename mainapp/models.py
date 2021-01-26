d3a9dd5065aa24f5ebe7befe1f6886523c0ee2a4
Sergei Makarenko <makarenko.s@inbox.ru>
2021-01-26 10:12:06 +0500

    Added slug fields in the model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.urls import reverse
+from django.template.defaultfilters import slugify
 from authapp.models import HabrUser
 
 
 class Category(models.Model):
     """Модель категории."""
-    title = models.CharField(verbose_name='Название категории', max_length=50)
+    title = models.CharField(verbose_name='Название категории', max_length=50, unique=True)
     description = models.CharField(verbose_name='Описание категории', max_length=255, blank=True)
+    slug = models.SlugField(null=False, unique=True)
     is_active = models.BooleanField(verbose_name='Категория активна', default=True)
     created_at = models.DateTimeField(verbose_name='Дата создания', auto_now=True)
 
@@ -15,6 +18,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('category_articles', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
 
 
 class Article(models.Model):
@@ -27,10 +38,11 @@ class Article(models.Model):
 
     user = models.ForeignKey(HabrUser, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(verbose_name='Название статьи', max_length=255)
+    title = models.CharField(verbose_name='Название статьи', max_length=255, unique=True)
     body = models.TextField(verbose_name='Содержимое статьи')
     image = models.ImageField(upload_to='article_image', blank=True)
     status = models.CharField(verbose_name='Статус статьи', max_length=50, choices=ARTICLE_STATUS)
+    slug = models.SlugField(null=False, unique=True)
     is_banned = models.BooleanField(verbose_name='Заблокировать статью', default=False)
     created_at = models.DateTimeField(verbose_name='Дата создания', auto_now=True)
     updated_at = models.DateTimeField(verbose_name='Дата обновления', auto_now=True)
@@ -41,3 +53,11 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('list_articles', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
