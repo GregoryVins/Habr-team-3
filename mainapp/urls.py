8ba85d25e723fe5c050299fd6e9caa3e7b633eea
Sergei Makarenko <makarenko.s@inbox.ru>
2021-01-28 09:26:53 +0500

    Added category detail view and filtering by date created

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from mainapp.views import ArticleListView, ArticleDetailView
+from mainapp.views import ArticleListView, ArticleDetailView, CategoryDetailView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='list_articles'),
-    path('category/<slug:slug>', ArticleListView.as_view(), name='category_articles'),
     path('article/<slug:slug>/', ArticleDetailView.as_view(), name='detail_article'),
+    path('category/<slug:slug>', CategoryDetailView.as_view(), name='category_articles'),
 ]
