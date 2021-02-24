00960c556fbaf36b081cc612a554d83c4b9babfc
Sergei Makarenko <makarenko.s@inbox.ru>
2021-02-24 23:58:06 +0300

    Added keyword search functionality. Added keyword highlighting

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from mainapp.views import ArticleListView, ArticleDetailView, CategoryDetailView, CreateCommentView, DeleteCommentView
+from mainapp.views import ArticleListView, ArticleDetailView, CategoryDetailView, \
+    CreateCommentView, DeleteCommentView, SearchView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='list_articles'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('category/<slug:slug>', CategoryDetailView.as_view(), name='category_articles'),
     path('article/comment/<int:pk>/', CreateCommentView.as_view(), name='create_comment'),
     path('article/comment/delete/<int:pk>/', DeleteCommentView.as_view(), name='delete_comment'),
+    path('search/', SearchView.as_view(), name='main_search'),
 ]
